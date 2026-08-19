bcb400c164afd2b465db90f271595c3a640f30f8
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-08-18 19:31:30 -0500

    feat: add tasks

---
+++ src/components/tasks/MicroTaskList.tsx
@@ -0,0 +1,24 @@
+import type { MicroTask } from "../../types";
+
+interface MicroTaskListProps {
+    microTasks: MicroTask[];
+    onToogle: (id:string) => void;
+}
+
+export function MicroTaskList({
+    microTasks,
+    onToogle
+}: MicroTaskListProps) {
+    return (
+        <div>
+            {microTasks.map((microTask) => (
+                <label key={microTask.id}>
+                    <input type="checkbox" checked={microTask.completed} />
+                    <span>
+                        {microTask.title}
+                    </span>
+                </label>
+            ))}
+        </div>
+    )
+} 
